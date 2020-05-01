f53f00fa654362d9fef421c095424f11047d5e92
DannyFrancis <64604163+DannyFrancis@users.noreply.github.com>
2020-05-01 10:57:00 +0800

    Version 1.1
    Updated the code to check on the sentences inside of a list in DetailView

--- ContentView.swift
+++ ContentView.swift
@@ -8,27 +8,11 @@
 
 import SwiftUI
 
-// MARK: - WelcomeDetailElement
 struct WelcomeDetailElement: Codable, Identifiable {
-        let name: String
-        let id: Int
-        let sentence: [String]
+    let name: String
+    let id: Int
+    let sentence: [String]
 }
-
-// MARK: - Sentence
-struct Sentence:Codable {
-        let sentence1, sentence2, sentence3, sentence4: String
-        let sentence5, sentence6, sentence7, sentence8: String
-        let sentence9, sentence10, sentence11, sentence12: String
-        let sentence1A, sentence1B, sentence2A, sentence2B: String?
-        let sentence3A, sentence3B, sentence4A, sentence4B: String?
-        let sentence5A, sentence5B, sentence6A, sentence6B: String?
-        let sentence7A, sentence7B, sentence8A, sentence8B: String?
-        let sentence9A, sentence9B, sentence10A, sentence10B: String?
-        let sentence11A, sentence11B, sentence12A, sentence12B: String?
-    
-}
-
 
 struct ContentView: View {
 
@@ -48,10 +32,12 @@ struct ContentView: View {
         }
         .onAppear(perform: parseJson)
     }
+        
+        
 
     func parseJson() {
 
-        if let url = Bundle.main.url(forResource: "twelveData", withExtension: "json"){
+        if let url = Bundle.main.url(forResource: "twelveData.json", withExtension: nil){
 
             if let data = try? Data(contentsOf: url){
 
@@ -71,15 +57,13 @@ struct ContentView: View {
 }
     
 struct DetailView: View {
-    @State var sentence: [String]
+@State var sentence: [String]
     var body: some View {
-        List{
-            ForEach(sentence, id:\.self){
-                s in Text(s)
-            }
+    List{
+        ForEach(sentence, id:\.self){
+            s in Text(s)
         }
     }
-
 }
 
 
@@ -88,6 +72,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
 
 
